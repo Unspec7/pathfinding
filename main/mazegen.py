5335a5569b68897674b5b63e43d806db27eb833a
Unspec <jeffrey.ma95@gmail.com>
2018-02-23 13:46:40 -0500

    Generating mazes

--- main/mazegen.py
+++ main/mazegen.py
@@ -1,10 +1,6 @@
-#THIS IS COPIED CODE FOR LEARNING PURPOSES
-
 from collections import defaultdict
-from contextlib import contextmanager
 from itertools import product
 import random
-import time
 from PIL import Image
 
 # List of cardinal directions.
@@ -12,7 +8,9 @@ _DIRECTIONS = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
 
 def generate_graph(size):
-    """Return size-by-size maze graph in the form of a mapping from vertex
+    """
+
+    Return size-by-size maze graph in the form of a mapping from vertex
     coordinates to sets of coordinates of neighbouring vertices, that is:
 
         graph[x, y] = {(x1, y1), (x2, y2), ...}
@@ -31,7 +29,9 @@ def generate_graph(size):
 
 
 def generate_maze(graph):
-    """Given a graph as returned by generate_graph, return the set of
+    """
+
+    Given a graph as returned by generate_graph, return the set of
     coordinates on the path in a random maze on that graph.
 
     """
@@ -54,7 +54,9 @@ def generate_maze(graph):
 
 
 def generate_image(filename, size, path):
-    """Create a size-by-size black-and-white image, with white on path and
+    """
+
+    Create a size-by-size black-and-white image, with white on path and
     black elsewhere, and save it to filename.
 
     """
@@ -65,30 +67,26 @@ def generate_image(filename, size, path):
     image.save(filename)
 
 
-@contextmanager
-def timer(message):
-    "Context manager that reports the time taken by a block of code."
-    print("[*]", message)
-    start = time.time()
-    yield
-    print("[#] finished in {:.3f} seconds".format(time.time() - start))
-
-
 def create(filename, size):
-    "Create a size-by-size maze and save it to filename."
+
+    """"
+
+    Create a size-by-size maze and save it to filename.
+
+    """
+
     ext = '.png'
     if not filename.endswith(ext):
         filename += ext
     size_real = (2 * size) + 1
-    with timer("creating {}".format(filename)):
-        print("size =", size_real, "x", size_real)
-        with timer("generating graph"):
-            graph = generate_graph(size)
-        with timer("generating maze"):
-            path = generate_maze(graph)
-        # Add entrance and exit.
-        path.update([(1, 0), (size_real - 1, size_real - 2)])
-        with timer("generating image"):
-            generate_image(filename, size_real, path)
+    graph = generate_graph(size)
+    path = generate_maze(graph)
 
-create("maze", 101)
+    # Ensure the squares the target and agent are on are clear
+    path.update([(1, 0), (size_real - 1, size_real - 2)])
+    generate_image(filename, size_real, path)
+
+
+for x in range(1, 51):
+    print ("Generating maze number", x)
+    create("maze" + str(x), 101)
