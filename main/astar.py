b2a38c6c499756e5bf9d924cab0797a33154fbf5
Brian <bma68@scarletmail.rutgers.edu>
2018-02-24 16:09:21 -0500

    Merge branch 'master' of https://github.com/Unspec7/pathfinding
    # Conflicts:
    #	main/astar.py

--- main/astar.py
+++ main/astar.py
@@ -1,10 +1,43 @@
-import heapq
+from heapq import *
 
-class AStarSeach(object):
-    def __init__(self):
-        self.opened = []
-        self.closed = set()
-        self.squares = []
-        self.grid_height = 101 #Keep in mind indexes run from 0 to 100
-        self.grid_width = 101
-        heapq.heapify(self.opened)
+class Square(object):
+    def __init__(self, x, y, valid):
+        self.x = x
+        self.y = y
+        self.valid = valid
+        self.parent = None
+        self.g = 0
+        self.h = 0
+        self.f = 0
+        self.visited = False
+
+
+def heuristic(curr, goal):
+    (x1, y1) = curr
+    (x2, y2) = goal
+    return abs(x1 - x2) + abs(y1 - y2)
+
+
+def a_star_search(graph, source, sink):
+    frontier = []
+    heappush(source, 0)
+    parent = {}
+    cost_so_far = {}
+    parent[source] = None
+    cost_so_far[source] = 0
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == sink:
+            break
+
+        for next in graph.neighbors(current):
+            new_cost = cost_so_far[current] + graph.cost(current, next)
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + heuristic(goal, next)
+                frontier.put(next, priority)
+                parent[next] = current
+
+    return parent, cost_so_far
