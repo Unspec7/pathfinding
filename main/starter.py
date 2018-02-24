9224590ce9dc31312e655ece3ac4cd8f24dcccdf
Brian <bma68@scarletmail.rutgers.edu>
2018-02-24 17:44:05 -0500

    Merge branch 'master' of https://github.com/Unspec7/pathfinding
    # Conflicts:
    #	main/astar.py

--- main/starter.py
+++ main/starter.py
@@ -1,16 +1,34 @@
 import pygame
 
 pygame.init()
+displaywidth = 800
+displayheight = 600
 
-gameDisplay = pygame.display.set_mode((800,600))
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+maze =
+
+gameDisplay = pygame.display.set_mode((displaywidth,displayheight))
 pygame.display.set_caption('Maze')
 clock = pygame.time.Clock()
 crash = False
+
+
+
 
 while not crash:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crash = True
+    gameDisplay.fill(white)
+    for i in range(0, 10):
+        for j in range(0, 10):
+            if maze(i, j).block is True:
+                pygame.draw.rect(gameDisplay, black, (i*5, j*5, 5, 5), 0)
+
     pygame.display.update()
     clock.tick(30)
 
@@ -47,3 +65,4 @@ def main(argv=None):
 
 if __name__ == "__main__":
     sys.exit(main())
+"""
