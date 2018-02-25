d65c0345998bdf4cb70e830dc4ead17472c82dde
Aniket Deshpande <anikeeet.deshpande@gmail.com>
2018-02-24 19:15:28 -0500

    A* backwards added

--- main/astar.py
+++ main/astar.py
@@ -36,7 +36,31 @@ def a_star_search(graph, source, sink):
             new_cost = cost_so_far[current] + graph.cost(current, next)
             if next not in cost_so_far or new_cost < cost_so_far[next]:
                 cost_so_far[next] = new_cost
-                priority = new_cost + heuristic(goal, next)
+                priority = new_cost + heuristic(sink, next)
+                frontier.put(next, priority)
+                parent[next] = current
+
+    return parent, cost_so_far
+
+def a_star_backwards(graph, source, sink):
+    frontier = []
+    heappush(sink, 0)
+    parent = {}
+    cost_so_far = {}
+    parent[sink] = None
+    cost_so_far[sink] = 0
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == source:
+            break
+
+        for next in graph.neighbors(current):
+            new_cost = cost_so_far[current] + graph.cost(current, next)
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + heuristic(source, next)
                 frontier.put(next, priority)
                 parent[next] = current
 
