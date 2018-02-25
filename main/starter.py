2617cd67baf00bc1bce2c188175dea7a12e4dab4
Jeff Ma <jeffrey.ma95@gmail.com>
2018-02-25 17:56:02 -0500

    Deleted crash

--- main/starter.py
+++ main/starter.py
@@ -94,9 +94,9 @@ def button(msg, x, y, w, h, ic, ac, action=None):
     click = pygame.mouse.get_pressed()
     if x + w > mouse[0] > x and y + h > mouse[1] > y:
         pygame.draw.rect(gameDisplay, ac, (x, y, w, h))
-
-        if click[0] == 1 and action != None:
-            action()
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                action()
     else:
         pygame.draw.rect(gameDisplay, ic, (x, y, w, h))
 
