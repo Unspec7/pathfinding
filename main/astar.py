0723a8970b644a3a80c8e260cc76775b34e41f3d
Brian Abrams <bma68@scarletmail.rutgers.edu>
2018-02-26 15:40:01 -0500

    A star works!

--- main/astar.py
+++ main/astar.py
@@ -41,8 +41,8 @@ def a_star_search(graph):
 
 def a_star_backwards(graph):
     graph.clean = False
-    source = graph.master[0][10]
-    sink = graph.master[100][100]
+    source = graph.master[100][100]
+    sink = graph.master[0][0]
     source.h = heuristic(source, sink)
     source.f = source.h
     openlist = []
@@ -63,6 +63,37 @@ def a_star_backwards(graph):
                 neighbor.g = current.g+1
                 neighbor.f = neighbor.g+neighbor.h
                 heappush(openlist, neighbor)
+                heappush(openlist, neighbor)
+    if sink.searchvisit is not True:
+        graph.path_found = False
+    return graph
+
+def adaptive_a_star_search(graph):
+    graph.clean = False
+    source = graph.master[0][0]
+    sink = graph.master[100][100]
+    source.h = heuristic(source, sink)
+    source.f = source.h
+    openlist = []
+    heappush(openlist, source)
+    while len(openlist) > 0:
+        current = heappop(openlist)
+        current.searchvisit = True
+        if current == sink:
+            graph.path_found = True
+            break
+        for items in current.neighbors:
+            neighbor = graph.master[items[0]][items[1]]
+            if neighbor.wall:
+                continue
+
+            '''don't need to check to see if openlist has shorter g value because g is always 1'''
+
+            if not neighbor.searchvisit and neighbor not in openlist:
+                neighbor.parent = current
+                neighbor.h = heuristic(neighbor, sink)
+                neighbor.g = current.g+1
+                neighbor.f = neighbor.g+neighbor.h
                 heappush(openlist, neighbor)
     if sink.searchvisit is not True:
         graph.path_found = False
